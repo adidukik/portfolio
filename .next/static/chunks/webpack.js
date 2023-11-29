04b4613d5a80234049ef2afc76186db46edbe9b8
Damir Zhumatayev <adidukik@gmail.com>
2023-11-29 16:04:12 +0900

    Add animated section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dc64191f086b1f30"; }
+/******/ 		__webpack_require__.h = function() { return "5f9d9d7bc0b2646f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
